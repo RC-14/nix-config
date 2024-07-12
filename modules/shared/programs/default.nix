b365a2a44989b3d73ea83ed4043d9149e85a3d57
RC-14 <61058098+RC-14@users.noreply.github.com>
2024-07-13 01:43:11 +0200

    Add descriptions for git shit and git wipe

--- modules/shared/programs/default.nix
+++ modules/shared/programs/default.nix
@@ -30,7 +30,9 @@ in {
 			pull.rebase = true;
 		};
 		aliases = {
+			# Undo the last commit, keeping all files and changes
 			shit = "reset --soft HEAD~1";
+			# Reset files to the state of the current commit. Does not delete untracked files, use clean -i for that.
 			wipe = "reset --hard";
 		};
 	};
