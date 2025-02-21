3e9571fdf8032fb12861ab3a776ec9e482c9cb9c
RC-14 <61058098+RC-14@users.noreply.github.com>
2025-02-21 01:45:37 +0100

    Add mpv keybinds for chapters

--- modules/shared/programs/mpv.nix
+++ modules/shared/programs/mpv.nix
@@ -7,6 +7,10 @@
 	bindings = {
 		# Skip anime opening
 		"Shift+s" = "seek +85";
+    # Next Chapter
+    "Alt+right" = "add chapter 1";
+    # Previous Chapter / Start of current Chapter
+    "Alt+left" = "add chapter -1";
 	};
 
 	config = {
@@ -19,6 +23,9 @@
 		vo = "libmpv";
 		# Use hardware video decoding
 		hwdec = "yes";
+
+    # Treat right Alt key as Alt not Alt-Gr
+    input-right-alt-gr = "no";
 
 		# Screenshot config
 		screenshot-format = "png";
